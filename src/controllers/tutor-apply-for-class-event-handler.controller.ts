468b5718ad2746af2bb2b2ef053b4a0f45494c25
Lephat902 <lehoangngocphat992002@gmail.com>
2024-03-01 19:29:03 +0700

    Listen for class created event to designate tutors

--- src/controllers/tutor-apply-for-class-event-handler.controller.ts
+++ src/controllers/tutor-apply-for-class-event-handler.controller.ts
@@ -1,15 +1,23 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
-import { ClassDeletedEventPattern, ClassDeletedEventPayload } from '@tutorify/shared';
+import { ClassCreatedEventPattern, ClassCreatedEventPayload, ClassDeletedEventPattern, ClassDeletedEventPayload } from '@tutorify/shared';
 import { TutorApplyForClassService } from 'src/tutor-apply-for-class.service';
 
 @Controller()
 export class TutorApplyForClassControllerEventHandler {
   constructor(private readonly tutorApplyForClassService: TutorApplyForClassService) { }
 
+  @EventPattern(new ClassCreatedEventPattern())
+  async handleClassCreated(payload: ClassCreatedEventPayload) {
+    console.log('Start designating tutors to a class (if any)')
+    const { classId, desiredTutorIds } = payload;
+    if (desiredTutorIds)
+      await this.tutorApplyForClassService.designateTutors(classId, desiredTutorIds);
+  }
+
   @EventPattern(new ClassDeletedEventPattern())
   async handleClassDeleted(payload: ClassDeletedEventPayload) {
-    console.log('Start setting all applications of this class to CLASS_DELETED (id any)')
+    console.log('Start setting all applications of this class to CLASS_DELETED (if any)')
     const { classId } = payload;
     this.tutorApplyForClassService.setAllApplicationToClassDeletedByClassId(classId);
   }
